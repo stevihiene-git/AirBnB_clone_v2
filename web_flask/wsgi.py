431408f652e8c2aaa22125dfea35ed510ca449d0
Stephen Oduh <stevihiene@gmail.com>
2024-06-12 09:21:42 +0000

    mm

--- web_flask/wsgi.py
+++ web_flask/wsgi.py
@@ -3,8 +3,8 @@
 
 The application listens on 0.0.0.0, port 5000.
 Routes:
-    /: Displays 'Hello HBNB!'
-"""
+        /: Displays 'Hello HBNB!'
+        """
 from flask import Flask
 
 app = Flask(__name__)
